29d2d54d450378a89bf40e210955708ce87151c9
Utsav Katharotiya <98468952+Utsav-7@users.noreply.github.com>
2023-05-11 11:41:37 +0530

    Add files via upload

---
+++ 02-Spring-boot-spring-core/01-constructor-injection/src/main/java/com/utsav/springcore/Coach.java
@@ -0,0 +1,6 @@
+package com.utsav.springcore;
+
+public interface Coach {
+    String getDailyWorkout();
+
+}
---
+++ 02-Spring-boot-spring-core/01-constructor-injection/src/main/java/com/utsav/springcore/DemoController.java
@@ -0,0 +1,27 @@
+package com.utsav.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DemoController {
+    //---------Step:3 define a private field for the dependency
+    private Coach myCoach;
+
+    // @Autowired annotation tells Spring to inject a dependency
+    // If you only have one constructor then @Autowired on constructor is optional
+    @Autowired
+    public DemoController(Coach theCoach){
+        myCoach=theCoach;
+    }
+
+    //---------Step:4 Add GetMapping for /dailyWorkout
+    @GetMapping("/dailyWorkout")
+    public String getDailWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+
+}
