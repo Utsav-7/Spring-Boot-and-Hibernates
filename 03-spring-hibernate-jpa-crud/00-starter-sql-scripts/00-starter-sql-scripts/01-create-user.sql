d5431ec925afc286657bd9dd068693d8756202d0
Utsav Katharotiya <98468952+Utsav-7@users.noreply.github.com>
2023-05-12 22:51:43 +0530

    Add files via upload

---
+++ 03-spring-hibernate-jpa-crud/00-starter-sql-scripts/00-starter-sql-scripts/01-create-user.sql
@@ -0,0 +1,6 @@
+-- Drop user first if they exist
+
+-- Now create user with prop privileges
+CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+
+GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
